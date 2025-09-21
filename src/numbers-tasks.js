3a6519a33ff477242da8fc87517dc14ad79ec8b7
ikclouds <ikclouds91@gmail.com>
2025-09-21 19:56:22 +0300

    feat: roundToPowerOfTen

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -199,8 +199,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
